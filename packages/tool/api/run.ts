6f23a21bebe280c0a7296fc941e2a3233c6c7216
sd0ric4 <1286518974@qq.com>
2025-06-23 21:14:01 +0800

    fix: remove debug log from runToolHandler

--- packages/tool/api/run.ts
+++ packages/tool/api/run.ts
@@ -27,7 +27,6 @@ export const runToolHandler = () =>
   s.route(contract.tool.run, async (args) => {
     const { toolId, inputs, systemVar } = args.body;
     const tool = getTool(toolId);
-    console.log('run tool', toolId, inputs, systemVar);
 
     if (!tool) {
       return {
