0162e62fb11a17e1c5aeac5dfdb393321c6b2f4c
Finley Ge <32237950+FinleyGe@users.noreply.github.com>
2025-07-04 11:49:18 +0800

    fix: middleware could be disabled (#90)

--- src/router/middleware/auth.ts
+++ src/router/middleware/auth.ts
@@ -3,19 +3,14 @@ import type { Request, Response, NextFunction } from 'express';
 // Auth token middleware for all API routes
 export const authTokenMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const authToken = req.headers.authtoken as string;
-  // Check if Authorization header exists
-  if (!authToken) {
-    return res.status(401).json({
-      message: 'Invalid token'
-    });
-  }
-
   // Validate token (customize this logic based on your requirements)
   const validToken = process.env.AUTH_TOKEN!;
   if (!validToken) {
     return next(); // if no token is set, skip this middleware
   }
-  if (authToken !== validToken) {
+
+  // Check if Authorization header exists
+  if (!authToken || authToken !== validToken) {
     return res.status(401).json({
       message: 'Invalid token'
     });
