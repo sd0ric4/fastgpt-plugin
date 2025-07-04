5c0cd53c2dede290651900f0a4250e69d459aa50
Archer <545436317@qq.com>
2025-07-04 09:53:42 +0800

    rename (#89)

--- modules/tool/packages/feishu/config.ts
+++ modules/tool/packages/feishu/config.ts
@@ -46,7 +46,7 @@ export default defineTool({
         {
           valueType: WorkflowIOValueTypeEnum.object,
           key: 'result',
-          label: 'Http Response'
+          label: 'result'
         }
       ]
     }
